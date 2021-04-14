788f2b1362c76cf4f1dece4c37f767067172ea4d
CalSch <cal.schwick@gmail.com>
2021-04-14 16:52:07 -0500

    Wrapped functions into helium and fixed effected files

--- game.js
+++ game.js
@@ -1,21 +1,21 @@
-let canvas = Canvas(400, 220, 'gameScreen')
-let guy = Rectangle(100,45,10,10,0,"#ff0000");
-let guy4 = Circle(100,-10,6,"#00e0b0");
-let guy2 = Circle(150,60,6,"#0000ff");
-let guy3 = Image(5,-50,100,100,0,"https://calsch.repl.co/src/logo.png");
-let camera = Camera(5,-50);
-let game = Game(canvas, 60);
+let canvas = helium.Canvas(400, 220, 'gameScreen')
+let guy = helium.Rectangle(100,45,10,10,0,"#ff0000");
+let guy4 = helium.Circle(100,-10,6,"#00e0b0");
+let guy2 = helium.Circle(150,60,6,"#0000ff");
+let guy3 = helium.Image(5,-50,100,100,0,"https://calsch.repl.co/src/logo.png");
+let camera = helium.Camera(5,-50);
+let game = helium.Game(canvas, 60);
 
-guy.addScript(Script(function() {
+guy.addScript(helium.Script(function() {
 	guy.y=Math.sin(game.frame/30)*40;
 }));
-guy2.addScript(Script(function() {
+guy2.addScript(helium.Script(function() {
 	guy2.y = game.toWorldSpace(game.mousePos).y;
 }));
-guy4.addScript(Script(function() {
+guy4.addScript(helium.Script(function() {
 	guy4.x = game.toWorldSpace(game.mousePos).x;
 }));
-guy3.addScript(Script(function() {
+guy3.addScript(helium.Script(function() {
 	if (game.pressedKeys.d) {
 		guy3.x+=0.3*game.deltaTime;
 	}
--- game2.js
+++ game2.js
@@ -1,4 +1,4 @@
-var game = Game(Canvas(200,100,"gameScreen"), 60)
-game.setCamera(Camera(0,0))
-	.addSprite(Rectangle(20,20,40,20,"#ff0000"))
+let game = helium.Game(helium.Canvas(200,100,"gameScreen"), 60)
+game.setCamera(helium.Camera(0,0))
+	.addSprite(helium.Rectangle(20,20,40,20,"#ff0000"))
 	.start()
